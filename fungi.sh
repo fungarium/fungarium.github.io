a4dfee58bd76f98afbf9672ddc69c34bfe124d81
Boris Lariushin <boris@lariushin.org>
2016-09-10 17:01:07 +0300

    + Трутовик киноварно-красный (Pycnoporus cinnabarinus); ++ Лисичка трубчатая (Cantharellus tubaeformis); + Леоция скользкая (Leotia lubrica); ++ Лисичка желтеющая (Cantharellus lutescens); + Кордицепс военный (Cordyceps militaris); ++ Биспорелла лимонная (Bisporella citrina); ++ Трутовик овечий (Albatrellus ovinus)

--- fungi.sh
+++ fungi.sh
@@ -1,5 +1,5 @@
 git add --all *
-git commit -m 'links and help with identification update'
+git commit -m '+ Трутовик киноварно-красный (Pycnoporus cinnabarinus); ++ Лисичка трубчатая (Cantharellus tubaeformis); + Леоция скользкая (Leotia lubrica); ++ Лисичка желтеющая (Cantharellus lutescens); + Кордицепс военный (Cordyceps militaris); ++ Биспорелла лимонная (Bisporella citrina); ++ Трутовик овечий (Albatrellus ovinus)'
 git reflog expire --expire=now --all
 git gc --force --aggressive --prune=now
 git repack -a -d
