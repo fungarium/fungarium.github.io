5a8192edc9a4e60be74596c7f104dd78b13bfb44
Boris Lariushin <boris@lariushin.org>
2017-06-14 20:40:14 +0300

    // 14.06.2017 ++ Тафриномицеты (Taphrinomycetes)\Тафриновые (Taphrinales)\Тафриновые (Taphrinaceae)\Тафрина черёмуховая (Taphrina padi) +++++ Пукциниомицеты (Pucciniomycetes) ++ Пукциниомицеты (Pucciniomycetes)\Пукциниевые (Pucciniales)\Колеоспоровые (Coleosporiaceae)\Хризомикса Воронина (Chrysomyxa woroninii) + Пецицомицеты (Pezizomycetes)\Пецицевые (Pezizales)\Пецицевые (Pezizaceae)\Пецица изменчивая (Peziza varia) + Миксомицеты (Myxomycetes)\Стемонитовые (Stemonitales)\Стемонитовые (Stemonitidaceae)\Стемонитис бурый (Stemonitis fusca) + Агарикомицеты (Agaricomycetes)\Гименохетовые (Hymenochaetales)\Гименохетовые (Hymenochaetaceae)\Трутовик, Инонотус скошенный, Чага (Inonotus obliquus) +++ Агарикомицеты (Agaricomycetes)\Агариковые (Agaricales)\Энтоломовые (Entolomataceae) ++ Агарикомицеты (Agaricomycetes)\Агариковые (Agaricales)\Энтоломовые (Entolomataceae)\Энтолома шершавоножковая (Entoloma hirtipes)

--- fixjpg.sh
+++ fixjpg.sh
@@ -2,3 +2,8 @@ shopt -s globstar nullglob
 for file in **/*.JPG; do
 	git mv -f -v "$file" "${file%.*}.jpg"
 done
+
+shopt -s globstar nullglob
+for file in **/*.JPG; do
+	mv "$file" "${file%.*}.jpg"
+done
--- fungi.sh
+++ fungi.sh
@@ -1,6 +1,6 @@
+cd /e/album_fungi/
 /e/album_fungi/fixjpg.sh
 /e/Fungarium/fixjpg.sh
-cd /e/album_fungi/
 git add --all *
 git commit --file=/e/fungi.txt
 git reflog expire --expire=now --all
